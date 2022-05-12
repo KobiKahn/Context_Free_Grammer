e13ef9cff37edad50b01d63510752cb543ef6183
kobi <kobikahn@gmail.com>
2022-05-11 20:10:47 -0400

    Making progress on dictionary

--- main.py
+++ main.py
@@ -9,22 +9,30 @@
 # N > "man" , "dog" , "cat" , "telescope" , "park"
 # P > "in" , "on" , "by" , "with"
 
-def parse_sentence(sentence, main_list):
+def parse_sentence(sentence, main_dict):
     sentence = sentence.split()
-
+    print(sentence)
+    sentence_keys = []
     for word in sentence:
-        for dict in main_list:
-            for list in dict.values():
-                if len(list) > 1:
-                    print(list)
-                    # for item in list[0]:
-                    #     if word.lower() == item.lower():
-                    #         print(word)
+        word = word.lower()
+        word_key = ''
+        for key1, value1 in main_dict.items():
+            # print(key, value)
+            for key2, value2 in value1.items():
+                # print(key2, value2)
+                value2 = [x.lower() for x in value2]
+                if word in value2:
+                    word_key = key2
+        sentence_keys.append(word_key)
+    print(sentence_keys)
+
+
+
 
 
 def main(filename, sentence):
-    main_list = [{"P": ['in', 'on', 'by', 'with']}, {'N': ['man', 'dog', 'cat', 'telescope', 'park']}, {'Det': ['a', 'an', 'the', 'my']}, {"NP": [ ['John', 'Mary', 'Bib'], [['Det', 'N', 'PP'], ['Det', 'N']] ]}, {'V': ['saw', 'ate', 'walked']}, {'PP': [[], ['P', 'NP']]}, {'VP': [[], [['V', 'NP', 'PP'], ['VP', 'PP']]]}, {'S': [[], ['NP', 'VP']]}]
+    main_dict = {'WORDS':{"P": ['in', 'on', 'by', 'with'], 'N': ['man', 'dog', 'cat', 'telescope', 'park'], 'Det': ['a', 'an', 'the', 'my'], "NP": ['John', 'Mary', 'Bob'], 'V': ['saw', 'ate', 'walked']}, 'DATA':{'NP': [['Det', 'N', 'PP'], ['Det', 'N']], 'PP': ['P', 'NP'], 'VP': [['V', 'NP', 'PP'], ['VP', 'PP']], 'S': ['NP', 'VP']}}
 
-    parse_sentence(sentence, main_list)
+    parse_sentence(sentence, main_dict)
 
 main('grammer.txt', 'Bob saw a dog in the park')
